9c65856c10ce57f689f55e38c2dbf9d6144cbf7d
Alexandros Ntigkaris <87975862+ntigkaris@users.noreply.github.com>
2025-12-14 18:42:28 +0200

    standardize classes' and functions' definition

--- src/utils/etl.py
+++ src/utils/etl.py
@@ -6,16 +6,14 @@ import pandas as pd
 
 from conf.config import cfg
 
-class DataObj:
+class DataObj(object):
     def __init__(self,filename):
-        super().__init__()
+        super(DataObj,self).__init__()
         self.filename = filename
-        self.xlsx_params = {
-                            'io':f'{cfg.rawdir}/{self.filename}',
+        self.xlsx_params = {'io':f'{cfg.rawdir}/{self.filename}',
                             'sheet_name':None,
                             'skiprows':0,
-                            'header':0
-                            }
+                            'header':0}
         self.sheetname = 'Στ. ΕΓΝΑΤΙΑΣ'
         self.keep_cols = ['SO2\nμg/m3','PM10\nμg/m3','PM2,5\nμg/m3','CO\nmg/m3','NO\nμg/m3','NO2\nμg/m3','O3\nμg/m3','Θερμοκ -\nρασία\no C','Σχετική\nΥγρασία\n%']
         self.cols = ['so2','pm10','pm25','co','no','no2','o3','temp','rh']
@@ -34,6 +32,7 @@ def integrate():
 
 def write(df):
     df.to_csv(f"{cfg.prodir}/poll_{'_'.join([re.sub(r'(?:poll_|.xlsx)','',f) for f in os.listdir(cfg.rawdir)])}.csv",mode='w',index=False)
+    pass
 
 def do_etl():
     write(integrate())
